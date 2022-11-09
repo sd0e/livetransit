41beb8c5408cf74e1053ee6cff2854409baf563c
Seb Doe <39373619+sd0e@users.noreply.github.com>
2022-11-09 22:02:08 +0000

    Update url to ensure future compatibility

--- src/scripts/GetMinibusStopRouteETA.js
+++ src/scripts/GetMinibusStopRouteETA.js
@@ -2,7 +2,7 @@ import XHRGetRequest from './XHRGetRequest';
 
 const GetMinibusStopRouteETA = (stopId, routeId) => {
 	const promise = new Promise(resolve => {
-		XHRGetRequest(`https://crossrun.herokuapp.com/https://data.etagmb.gov.hk/eta/stop/${stopId}`).then(res => {
+		XHRGetRequest(`https://crossrun.onrender.com/https://data.etagmb.gov.hk/eta/stop/${stopId}`).then(res => {
 			res = res.data;
 			res.forEach(routeData => {
 				if (routeData.route_id === routeId) {
